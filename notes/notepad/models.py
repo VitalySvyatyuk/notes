8e01d1a060a6dc596c86fe51093cf64d7b7b7d46
VITALIY SVYATUK <vitaliysvyatuk@VITALIYs-MacBook.local>
2016-11-30 13:09:19 +0300

    notes

--- notes/notepad/models.py
+++ notes/notepad/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    name = models.CharField(max_length=30)
+    text = models.CharField(max_length=3000)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'notes'
--- notes/notepad/views.py
+++ notes/notepad/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
+                                                                 
+from .models import Note
+
 
 def index(request):
 	return render(request, 'index.html', {})
 
-
-
+@login_required
+def mynotes(request):
+	notes = Note.objects.all()
+	return render(request, 'mynotes.html', { 'notes': notes })
 
 # def logout(request):
 #     logout(request)
@@ -14,5 +19,4 @@ def index(request):
 
 
 
-# @login_required
-# def my_notes(request):
+
